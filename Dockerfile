a0dc46d29c9216a43cfddb7f9935fc959341bb00
lkm454545 <lkm454545@naver.com>
2023-07-27 09:46:37 +0900

    docker change

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,4 @@ ARG JAR_FILE=/build/libs/back-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} /app.jar
 
 ENTRYPOINT ["java","-jar", "/app.jar"]
+
--- src/main/java/com/example/back/controller/HelloController.java
+++ src/main/java/com/example/back/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.back.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
+    public String come() {
+        return "welcome!!";
+    }
+
     @GetMapping("/api/hello")
     public String hello() {
+        log.info("################################# hello #########################");
         return "hello";
     }
 
